0b1ac0e4534f34b0295807077c7832719d8698b7
Gabriel Alejandro Lopez Rueda <galopez@eeq.com>
2025-12-30 09:21:38 -0500

    creacion de test unitarios de barbero service y controller

--- src/barbero/test/barbero.controller.spec.ts
+++ src/barbero/test/barbero.controller.spec.ts
@@ -1,20 +1,104 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { BarberoController } from '../barbero.controller';
 import { BarberoService } from '../barbero.service';
+import { BarberSchema } from '../../schemas/barber.schema';
+
+const mockBarberoService = {
+  createBarbero: jest.fn(),
+  findAllBarber: jest.fn(),
+  findOneBarber: jest.fn(),
+  updateBarber: jest.fn(),
+  removeBarber: jest.fn(),
+};
+const mockBarbero: BarberSchema = {
+  idBarber: 'uuid-1',
+  dniBarber: '1751459056',
+  nameBarber: 'Gabriel',
+  cita: [],
+};
 
 describe('BarberoController', () => {
   let controller: BarberoController;
+  let service: BarberoService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [BarberoController],
-      providers: [BarberoService],
+      providers: [
+        {
+          provide: BarberoService,
+          useValue: mockBarberoService,
+        },
+      ],
     }).compile();
 
     controller = module.get<BarberoController>(BarberoController);
+    service = module.get<BarberoService>(BarberoService);
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+  it('should create a barbero', async () => {
+    const dto = {
+      dniBarber: '1751459056',
+      nameBarber: 'Gabriel',
+    };
+    mockBarberoService.createBarbero.mockResolvedValue(mockBarbero);
+
+    const result = await controller.createBarbero(dto);
+
+    expect(service.createBarbero).toHaveBeenCalledWith(dto);
+    expect(result).toEqual(mockBarbero);
+  });
+  it('should find All barbero', async () => {
+    mockBarberoService.findAllBarber.mockReturnValue([mockBarbero]);
+
+    const result = await controller.findAll();
+
+    expect(service.findAllBarber).toHaveBeenCalled();
+    expect(result).toEqual([mockBarbero]);
+  });
+  it('should find BY ID barbero ', async () => {
+    const id = 'uuid-1';
+    mockBarberoService.findOneBarber.mockResolvedValue(mockBarbero);
+    const result = await controller.findOneById(id);
+
+    expect(service.findOneBarber).toHaveBeenCalledWith(id);
+    expect(result).toEqual(mockBarbero);
+  });
+  it('should update Barbero', async () => {
+    const id = 'uuid-1';
+    const updateDto = { dniBarber: '1751459056', nameBarber: 'Gabriel' };
+
+    const responseMock = {
+      mensaje: `Barbero con numero de id: ${id}, fue actualizado`,
+      barberActualizada: {
+        ...mockBarbero,
+
+        nameBarber: 'Gabriel',
+      },
+    };
+    mockBarberoService.updateBarber.mockResolvedValue(responseMock);
+
+    const result = await controller.updateBarber(id, updateDto);
+
+    expect(service.updateBarber).toHaveBeenCalledWith(id, updateDto);
+    expect(result).toEqual(responseMock);
+  });
+  it('should remove barbero by id', async () => {
+    const id = 'uuid-1';
+
+    const responseMock = {
+      mensaje: `barber con numero de id ${id}, eliminado`,
+      barber: mockBarbero,
+    };
+
+    mockBarberoService.removeBarber.mockResolvedValue(responseMock);
+
+    const result = await controller.removeBarber(id);
+
+    expect(service.removeBarber).toHaveBeenCalledWith(id);
+    expect(result).toEqual(responseMock);
   });
 });
--- src/barbero/test/barbero.service.spec.ts
+++ src/barbero/test/barbero.service.spec.ts
@@ -3,12 +3,14 @@ import { BarberoService } from '../barbero.service';
 import { Repository } from 'typeorm';
 import { BarberSchema } from '../../schemas/barber.schema';
 import { getRepositoryToken } from '@nestjs/typeorm';
-import { BadRequestException } from '@nestjs/common';
+import { BadRequestException, NotFoundException } from '@nestjs/common';
 
 const mockBarberoRepository = {
   findOne: jest.fn(),
   create: jest.fn(),
   save: jest.fn(),
+  find: jest.fn(),
+  remove: jest.fn(),
 };
 
 describe('BarberoService', () => {
@@ -16,6 +18,7 @@ describe('BarberoService', () => {
   let repository: Repository<BarberSchema>;
 
   beforeEach(async () => {
+    jest.clearAllMocks();
     const module: TestingModule = await Test.createTestingModule({
       providers: [
         BarberoService,
@@ -62,25 +65,151 @@ describe('BarberoService', () => {
     expect(result).toEqual(barberMock);
   });
 
-
   it('should throw error if dni already exists', async () => {
-  const dto = {
-    dniBarber: '1751459056#',
-    nameBarber: 'Gabriel',
-  };
+    const dto = {
+      dniBarber: '1751459056#',
+      nameBarber: 'Gabriel',
+    };
 
-  const barberoExistente ={
-    idBarbero: 'uuid-existing',
-    dniBarber: '1751459056#',
-    nameBarber: 'Otro Barbero',
-    cita: []
-  };
+    mockBarberoRepository.findOne.mockResolvedValue({
+      idBarbero: 'uuid-existing',
+    });
 
-  mockBarberoRepository.findOne.mockResolvedValue({
-    idBarbero: 'uuid-existing',
+    await expect(service.createBarbero(dto)).rejects.toThrow(
+      BadRequestException,
+    );
   });
+  it('should return all barberos', async () => {
+    const barberoMock = [
+      {
+        idBarbero: 'uuid-1',
+        dniBarbero: '1751459056',
+        nameBarbero: 'Juan',
+        cita: [],
+      },
+      {
+        idBarbero: 'uuid-2',
+        dniBarber: '456',
+        nameBarber: 'Pedro',
+        cita: [],
+      },
+    ];
+    mockBarberoRepository.find.mockResolvedValue(barberoMock);
+    const result = await service.findAllBarber();
 
-  await expect(service.createBarbero(dto)).rejects.toThrow(BadRequestException);
-});
+    expect(mockBarberoRepository.find).toHaveBeenCalled();
+    expect(result).toEqual(barberoMock);
+  });
+  it('should return one barberos', async () => {
+    const id = 'uuid-1';
 
+    const barberoMock = {
+      idBarber: 'uuid-1',
+      dniBarbero: '1751459056',
+      nameBarbero: 'Juan',
+      cita: [],
+    };
+
+    mockBarberoRepository.findOne.mockResolvedValue(barberoMock);
+    const result = await service.findOneBarber(id);
+
+    expect(mockBarberoRepository.findOne).toHaveBeenCalledWith({
+      where: { idBarber: id },
+    });
+    expect(result).toEqual(barberoMock);
+  });
+  it('should throw NotFoundException if barbero does not exist', async () => {
+    const id = 'uuid-inexistente';
+    mockBarberoRepository.findOne.mockResolvedValue(null);
+
+    await expect(service.findOneBarber(id)).rejects.toThrow(NotFoundException);
+
+    expect(mockBarberoRepository.findOne).toHaveBeenCalledWith({
+      where: { idBarber: id },
+    });
+  });
+  it('should update barbero successfully', async () => {
+    const id = 'uidd-1';
+    const barberoExiste = {
+      idBarber: id,
+      dniBarber: '1751459056',
+      nameBarber: 'Juan',
+      cita: [],
+    };
+    const updateDto = {
+      dniBarber: '1751459056#',
+      nameBarber: 'Gabriel',
+    };
+
+    const barberoActualizado = {
+      ...barberoExiste,
+      nameBarber: 'Gabriel',
+    };
+
+    mockBarberoRepository.findOne.mockResolvedValue(barberoExiste);
+    mockBarberoRepository.save.mockResolvedValue(barberoActualizado);
+
+    const result = await service.updateBarber(id, updateDto);
+
+    expect(mockBarberoRepository.findOne).toHaveBeenCalledWith({
+      where: { idBarber: id },
+    });
+
+    expect(mockBarberoRepository.save).toHaveBeenCalledWith({
+      ...barberoExiste,
+      nameBarber: 'Gabriel',
+    });
+
+    expect(result).toEqual({
+      mensaje: `Barbero con numero de id: ${id}, fue actualizado`,
+      barberActualizada: barberoActualizado,
+    });
+  });
+  it('should throw NotFoundException if barbero does not exists', async () => {
+    const id = 'uidd-1';
+    mockBarberoRepository.findOne.mockResolvedValue(null);
+
+    await expect(
+      service.updateBarber(id, {
+        dniBarber: '1751459056#',
+        nameBarber: 'Gabriel',
+      }),
+    ).rejects.toThrow(NotFoundException);
+
+    expect(mockBarberoRepository.findOne).toHaveBeenCalledWith({
+      where: { idBarber: id },
+    });
+    expect(mockBarberoRepository.save).not.toHaveBeenCalled();
+  });
+  it('should remove barbero by id', async () => {
+    const id = 'uuid-1';
+    const barberMock = {
+      idBarber: id,
+      dniBarber: '54975855',
+      nameBarber: 'Gabriel',
+      cita: [],
+    };
+    mockBarberoRepository.findOne.mockResolvedValue(barberMock);
+    mockBarberoRepository.remove.mockResolvedValue(barberMock);
+    const result = await service.removeBarber(id);
+
+    expect(mockBarberoRepository.findOne).toHaveBeenCalledWith({
+      where: { idBarber: id },
+    });
+    expect(mockBarberoRepository.remove).toHaveBeenCalledWith(barberMock);
+
+    expect(result).toEqual({
+      mensaje: `barber con numero de id ${id}, eliminado`,
+      barber: barberMock,
+    });
+  });
+  it('should throw new NotFoundException when id already exists ', async () => {
+    const id = 'uuid-1';
+
+    mockBarberoRepository.findOne.mockResolvedValue(null);
+
+    await expect(service.removeBarber(id)).rejects.toThrow(NotFoundException);
+
+    expect(mockBarberoRepository.remove).not.toHaveBeenCalled();
+  });
 });
